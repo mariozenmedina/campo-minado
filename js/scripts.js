95969b09c22af9a950cef8103e09167331989ef8
mariozenmedina <mazen.mario@gmail.com>
2023-01-22 20:11:14 -0300

    morreu

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ let app = document.getElementById('app');
 let boardSize = [8, 8];
 let fields = boardSize.reduce((r, c) => r * c);
 let map = Array(fields); map.fill(0, 0, fields);
-let bombs = 60;
+let bombs = 10;
 
 function runBoard() {
 
@@ -12,6 +12,10 @@ function runBoard() {
     displayData();
 
     function setFields() {
+
+        app.style.minWidth = 'calc(40px * '+boardSize[1]+')';
+        app.style.width = 'calc(40px * '+boardSize[1]+')';
+
         for (var i = 0; i < fields; i++) {
             let div = document.createElement('div');
             div.classList.add('field');
@@ -46,7 +50,6 @@ function runBoard() {
                 adjacentes.push(i+columns-1);
             }
             if( ( i+1 ) % columns != 0){ //SE O ELEMENTO NÃO ESTÁ NA COLUNA DA DIREITA
-                console.log(i);
                 adjacentes.push(i+1);
                 adjacentes.push(i-columns+1);
                 adjacentes.push(i+columns+1);
@@ -69,17 +72,39 @@ function runBoard() {
         for (var i = 0; i < map.length; i++) {
             switch (map[i]) {
                 case 'b':
-                    divs[i].classList.add('bomba');
+                    divs[i].setAttribute('data-class', 'bomba');
                     break;
 
                 default:
-                    divs[i].classList.add('num', 'num' + map[i]);
+                    divs[i].setAttribute('data-class', 'num num' + map[i]);
                     break;
             }
+
+            divs[i].onclick = clickField;
         }
 
     }
 
+    function clickField(e){
+        let dataClass = e.target.getAttribute('data-class').split(' ');
+        for(var i=0; i<dataClass.length; i++){
+            e.target.classList.add(dataClass[i]);
+        }
+        if(dataClass.includes('bomba')){
+            morreu();
+        }
+    }
+
+    function morreu(){
+        let divs = document.querySelectorAll('#app .field');
+        for (var i = 0; i < divs.length; i++) {
+            divs[i].onclick = null;
+            let dataClass = divs[i].getAttribute('data-class').split(' ');
+            for(var j=0; j<dataClass.length; j++){
+                divs[i].classList.add(dataClass[j]);
+            }
+        }
+    }
 
 }
 
