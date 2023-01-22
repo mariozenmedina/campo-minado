cc9f2d832d1143f194529e959b1e01af6b9a7a4e
emagioli <enzomagioli@gmail.com>
2023-01-22 18:56:46 -0300

    erro setNumbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,91 @@
-function runBoard() {
-    //Variables
-    let app = document.getElementById('app');
-    let boardSize = [8, 8];
+//Variables
+let app = document.getElementById('app');
+let boardSize = [8, 8];
+let fields = boardSize.reduce((r, c) => r * c);
+let map = Array(fields); map.fill(0, 0, fields);
+let bombs = 10;
 
-    setFields(){
-        
+function runBoard() {
+
+    setFields();
+    setMap();
+    displayData();
+
+    function setFields() {
+        for (var i = 0; i < fields; i++) {
+            let div = document.createElement('div');
+            div.classList.add('field');
+            div.setAttribute('data-num', i);
+            app.append(div);
+        }
     }
+
+    function setMap() {
+        for (var i = 0; i < bombs; i++) {
+            let num = Math.floor(Math.random() * 63);
+            while (map[num]) {
+                num = Math.floor(Math.random() * 63);
+            }
+            map[num] = 'b';
+        }
+
+        for (var i = 0; i < map.length; i++) {
+            if (map[i] === 'b') {
+                setNumbers(i);
+            }
+        }
+
+        function setNumbers(i) {
+            let columns = boardSize[1];
+
+            if (map[i - columns - 1] != undefined && map[i - columns - 1] != 'b') {
+                map[i - columns - 1]++;
+            }
+            if (map[i - columns] != undefined && map[i - columns] != 'b') {
+                map[i - columns]++;
+            }
+            if (map[i - columns + 1] != undefined && map[i - columns + 1] != 'b') {
+                map[i - columns + 1]++;
+            }
+            if (map[i - 1] != undefined && map[i - 1] != 'b') {
+                map[i - 1]++;
+            }
+            if (map[i + 1] != undefined && map[i + 1] != 'b') {
+                map[i + 1]++;
+            }
+            if (map[i + columns - 1] != undefined && map[i + columns - 1] != 'b') {
+                map[i + columns - 1]++;
+            }
+            if (map[i + columns] != undefined && map[i + columns] != 'b') {
+                map[i + columns]++;
+            }
+            if (map[i + columns + 1] != undefined && map[i + columns + 1] != 'b') {
+                map[i + columns + 1]++;
+            }
+
+        }
+
+
+    }
+
+    function displayData() {
+        let divs = document.querySelectorAll('#app .field');
+
+        for (var i = 0; i < map.length; i++) {
+            switch (map[i]) {
+                case 'b':
+                    divs[i].classList.add('bomba');
+                    break;
+
+                default:
+                    divs[i].classList.add('num', 'num' + i);
+                    break;
+            }
+        }
+
+    }
+
+
 }
+
+runBoard();
